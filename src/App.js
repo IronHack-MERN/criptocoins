aaedc948cfc7cfe581becbc625889837655a606e
Elena Piaggio <elenapiaggio@gmail.com>
2020-08-09 19:04:18 +0200

    ADD styled components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
+import image from './criptocoins.png';
 
 const Container = styled.div`
   max-width: 900px;
   margin: 0 auto;
   @media (min-width: 992px){
-    display: grid:
+    display: grid;
     grid-template-columns: repeat(2, 1fr);
     column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #FFF;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
   }
 `;
 
@@ -15,7 +39,15 @@ function App() {
   return (
     <Container>
       <div>
-        <div></div>
+        <Image
+          src={image}
+          alt='imagen_of_criptocoins'
+        />
+      </div>
+      <div>
+        <Heading>
+          Instant coin quote
+        </Heading>
       </div>
     </Container>
   );
