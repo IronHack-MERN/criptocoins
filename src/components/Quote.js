662386c7d903cf4c823bb699c6ed12e4bbee705f
Elena Piaggio <elenapiaggio@gmail.com>
2020-08-21 23:05:16 +0200

    ADD styled components to Quote component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React from "react";
+import styled from "@emotion/styled";
 
+const ResultDiv = styled.div`
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: bold;
+  }
+`;
+const Price = styled.p`
+  font-size: 30px;
+  span {
+    font-weight: bold;
+  }
+`;
 const Quote = ({ result }) => {
-  if(Object.keys(result).length === 0){
-    return null
+  if (Object.keys(result).length === 0) {
+    return null;
   }
 
-  console.log("RESULT IN QUOTE IS", result);
-  return ( 
-    <div>
-      <p>Price is: <span>{result.PRICE}</span></p>
-      <p>Highday: <span>{result.HIGHDAY}</span></p>
-      <p>Lowday: <span>{result.LOWDAY}</span></p>
-      <p>Change last 24 hours: <span>{result.CHANGEPCT24HOUR}</span></p>
-      <p>Last update: <span>{result.LASTUPDATE}</span></p>
-    </div>
+  return (
+    <ResultDiv>
+      <Price>
+        Price is: <span>{result.PRICE}</span>
+      </Price>
+      <Info>
+        Highday: <span>{result.HIGHDAY}</span>
+      </Info>
+      <Info>
+        Lowday: <span>{result.LOWDAY}</span>
+      </Info>
+      <Info>
+        Change last 24 hours: <span>{result.CHANGEPCT24HOUR}</span>
+      </Info>
+      <Info>
+        Last update: <span>{result.LASTUPDATE}</span>
+      </Info>
+    </ResultDiv>
   );
-}
- 
+};
+
 export default Quote;
