b510fc93566fea439f949ad3fb59157f6a47b0e1
Elena Piaggio <elenapiaggio@gmail.com>
2020-08-21 23:51:48 +0200

    add conditional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
-import image from './criptocoins.png';
-import Form from './components/Form';
-import criptocoinsService from './services/criptocoinsService';
-import Quote from './components/Quote';
+import image from "./criptocoins.png";
+import Form from "./components/Form";
+import criptocoinsService from "./services/criptocoinsService";
+import Quote from "./components/Quote";
+import Spinner from "./components/Spinner";
 
 const Container = styled.div`
   max-width: 900px;
   margin: 0 auto;
-  @media (min-width: 992px){
+  @media (min-width: 992px) {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     column-gap: 2rem;
@@ -21,64 +22,63 @@ const Image = styled.img`
 `;
 
 const Heading = styled.h1`
-  font-family: 'Bebas Neue', cursive;
-  color: #FFF;
+  font-family: "Bebas Neue", cursive;
+  color: #fff;
   text-align: left;
   font-weight: 700;
   font-size: 50px;
   margin-bottom: 50px;
   margin-top: 80px;
 
-  &::after{
-    content: '';
+  &::after {
+    content: "";
     width: 100px;
     height: 6px;
-    background-color: #66A2FE;
+    background-color: #66a2fe;
     display: block;
   }
 `;
 
 function App() {
-  const [ coin, setCoin ] = useState('');
-  const [ criptocoin, setCriptocoin ] = useState('');
-  const [ result, setResult ] = useState({});
+  const [coin, setCoin] = useState("");
+  const [criptocoin, setCriptocoin] = useState("");
+  const [result, setResult] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  async function getData(){
+  async function getData() {
     try {
       const result = await criptocoinsService.getFullData(coin, criptocoin);
-      setResult(result.DISPLAY[criptocoin][coin]);
+
+      setLoading(true);
+
+      setTimeout(() => {
+        setLoading(false);
+        setResult(result.DISPLAY[criptocoin][coin]);
+      }, 3000);
     } catch (error) {
-      console.log('Not connection possible!!!');
+      console.log("Not connection possible!!!");
     }
   }
 
-  useEffect( () => {
-    if(coin === ''){
+  useEffect(() => {
+    if (coin === "") {
       return;
     }
 
     getData();
-  }, [coin, criptocoin])
+  }, [coin, criptocoin]);
+
+  const component = loading ? <Spinner /> : <Quote result={result} />;
 
   return (
     <Container>
       <div>
-        <Image
-          src={image}
-          alt='imagen_of_criptocoins'
-        />
+        <Image src={image} alt="imagen_of_criptocoins" />
       </div>
       <div>
-        <Heading>
-          Instant coin quote
-        </Heading>
-        <Form
-          setCoin={setCoin}
-          setCriptocoin={setCriptocoin}
-        />
-        <Quote
-          result = {result}
-        />
+        <Heading>Instant coin quote</Heading>
+        <Form setCoin={setCoin} setCriptocoin={setCriptocoin} />
+        {component}
       </div>
     </Container>
   );
